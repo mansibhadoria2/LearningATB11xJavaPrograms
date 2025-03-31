c17c4c63cf505c28dad1e62a93cde834570f4b4e
mansibhadoria2 <mansibhadoria2@gmail.com>
2025-03-31 17:59:07 +0530

    description of Lab)2_CommonMath

--- src/ex_01_Java_Basic/Lab02_CommonMath.java
+++ src/ex_01_Java_Basic/Lab02_CommonMath.java
@@ -2,10 +2,10 @@ package ex_01_Java_Basic;
 
 public class Lab02_CommonMath {
     public static void main(String[] args) {
-        System.out.println(5+2);
-        System.out.println(5-2);
-        System.out.println(5*2);
-        System.out.println(5/2);
-        System.out.println(5%2);
+        System.out.println(5+2); //Addition
+        System.out.println(5-2); //subtraction
+        System.out.println(5*2); // multiplication
+        System.out.println(5/2); // Division
+        System.out.println(5%2);  //Modulus
     }
 }
