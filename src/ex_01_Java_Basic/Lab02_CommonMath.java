0fdd43398d2294df6737ac37efb9c5a8e2aae435
mansibhadoria2 <mansibhadoria2@gmail.com>
2025-03-31 17:53:59 +0530

    Lab01_keyword_Identifier

---
+++ src/ex_01_Java_Basic/Lab02_CommonMath.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab02_CommonMath {
+    public static void main(String[] args) {
+        System.out.println(5+2);
+        System.out.println(5-2);
+        System.out.println(5*2);
+        System.out.println(5/2);
+        System.out.println(5%2);
+    }
+}
