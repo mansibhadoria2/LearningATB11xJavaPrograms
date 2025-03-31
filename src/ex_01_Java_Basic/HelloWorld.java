e42efa5c564e5a3f92687553ea5e96d70b62249e
mansibhadoria2 <mansibhadoria2@gmail.com>
2025-03-31 17:49:24 +0530

    Lab01_keyword_Identifier

--- src/ex_01_Java_Basic/HelloWorld.java
+++ src/ex_01_Java_Basic/HelloWorld.java
@@ -1,4 +1,11 @@
 package ex_01_Java_Basic;
 
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World..!");
+     /**
+     this is a multi comment line, it is getting ignored
+      */
+        System.out.println("This is line is printing after multi comment line");
+    }
 }
