eb4fccece9ba5a98398d9970b51b29d586c20641
mansibhadoria2 <mansibhadoria2@gmail.com>
2025-03-31 17:49:24 +0530

    Lab01_keyword_Identifier

---
+++ src/ex_01_Java_Basic/Lab01_keyword_Identifier.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;          //ex_01_Java_Basic -> Identifier
+
+public class Lab01_keyword_Identifier {          //Lab01_keyword_Identifier -> Identifier
+    public static void main(String[] args) {
+        System.out.println("Hello, Keywords..!!");
+
+    }
+}
